30663bfbaccb9f8807ed29947646c066294f7822
un0rsk <un0rsk@users.noreply.github.com>
2017-07-05 15:01:56 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Card from './Card';
+import Typist from './Typist';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React!</h2>
-			<Card/>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+			  <Typist name='Hi there, I am Donna'/>
       </div>
     );
   }
---
+++ src/Typist.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+
+
+class Typist extends Component {
+  delay =  150
+
+  constructor(props) {
+    super(props)
+    this.state = {typed: '', iterations: 0, typoAt: this.getTypoPosition(this.props.name)}
+  }
+
+  getTypoPosition(toType) {
+    const typoIndex = Math.floor(Math.random() * (toType.length - 1 - toType.length / 2) + toType.length / 2)
+    return typoIndex
+  }
+
+  charToTypeo(index) {
+    if (index === this.state.typoAt) {
+      return this.props.name.substr(index + 1, 1)
+    } else if (index === this.state.typoAt + 1) {
+      return this.props.name.substr(index - 1, 1)
+    } else {
+      return this.props.name.substr(index, 1)
+    }
+  }
+
+  printPart() {
+    if (this.state.iterations <= this.props.name.length) {
+      this.setState({
+        typed: this.state.typed + this.charToTypeo(this.state.iterations),
+        iterations: this.state.iterations + 1
+      })
+      setTimeout(() => this.printPart(), this.delay)
+    } else if (this.state.typoAt > 0) {
+      this.rollbackToTypo()
+    } else {
+      this.setState({disabled: true})
+    }
+  }
+
+  rollbackToTypo() {
+    if (this.state.iterations >= this.state.typoAt) {
+      this.setState({
+        typed: this.state.typed.substr(0, this.state.iterations),
+        iterations: this.state.iterations - 1
+      })
+      setTimeout(() => this.rollbackToTypo(), this.delay)
+    } else {
+      this.setState({typoAt: -1, iterations: this.state.iterations + 1})
+      this.printPart()
+    }
+  }
+
+  componentDidMount() {
+    this.typist.focus();
+    this.printPart()
+  }
+
+  render() {
+    return (
+      <div className='Typist'>
+        <input ref={(i) => this.typist = i} type='text' value={this.state.typed} disabled={this.disabled}/>
+      </div>
+    );
+  }
+}
+
+export default Typist;
